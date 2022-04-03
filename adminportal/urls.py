822e49c27538a823075b9360448423a03298833b
Ronak <ronakmohata@gmail.com>
2022-04-03 16:48:24 +0530

    fixed adminportal glitch

--- adminportal/urls.py
+++ adminportal/urls.py
@@ -7,7 +7,7 @@ app_name = 'adminportal'
 
 urlpatterns = [
     path('mail', sendMail.as_view(), name='mail'),
-    url(r'teams$', dashboardEsportsTeams, name='deteams'),
+    url(r'teamsEsports$', dashboardEsportsTeams, name='deteams'),
     url(r'teams$', dashboardTeams, name='dteams'),
     url(r'users$', dashboardUsers, name='dusers'),
     url(r'excel$', downloadExcel, name='teamInfo'),
