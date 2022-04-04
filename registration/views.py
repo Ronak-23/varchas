3194c317f835283725262611b73eba2b7f0b9f0c
Ronak <ronakmohata@gmail.com>
2022-04-04 22:35:40 +0530

    opened registration for all the sports

--- registration/views.py
+++ registration/views.py
@@ -20,19 +20,19 @@ class TeamFormationView(CreateView):
         if user is not None:
             form = TeamRegistrationForm(self.request.POST)
             user = get_object_or_404(UserProfile, user=user)
-            if user.teamId is not None:
-                message = "You are already in team {}".format(user.teamId)
-                message += "\nYou have to register again to join another team. \nContact Varchas administrators."
-                return HttpResponse(message, content_type="text/plain")
-            team = form.save()
-            if team.sport == '5':
-                message = "Registration for Cricket has been closed."
-                team.delete()
-                return HttpResponse(message, content_type="text/plain")
-            if ((team.sport == '3' or team.sport == '9') and user.gender == 'M'):
-                message = "Registration for Volleyball(M) and basketball(M) has been closed."
-                team.delete()
-                return HttpResponse(message, content_type="text/plain")
+            # if user.teamId is not None:
+            #     message = "You are already in team {}".format(user.teamId)
+            #     message += "\nYou have to register again to join another team. \nContact Varchas administrators."
+            #     return HttpResponse(message, content_type="text/plain")
+            # team = form.save()
+            # if team.sport == '5':
+            #     message = "Registration for Cricket has been closed."
+            #     team.delete()
+            #     return HttpResponse(message, content_type="text/plain")
+            # if ((team.sport == '3' or team.sport == '9') and user.gender == 'M'):
+            #     message = "Registration for Volleyball(M) and basketball(M) has been closed."
+            #     team.delete()
+            #     return HttpResponse(message, content_type="text/plain")
             # if team.sport == '6':
             #     message = "Registration for Football has been closed."
             #     team.delete()
