ff2bf8b79512df8206d7c0726d9662414dda47ef
Ronak <ronakmohata@gmail.com>
2022-05-23 15:05:37 +0530

    removed few files

---
+++ registration/serializers.py
@@ -0,0 +1,15 @@
+from .models import TeamRegistration
+from rest_framework import serializers
+
+
+class TeamsSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="registration:teamregistration-detail")
+    class Meta:
+        model = TeamRegistration
+        fields = ['url', 'teamId', 'college', 'sport', 'score']
+
+class TeamsIdSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="registration:teamregistration-detail")
+    class Meta:
+        model = TeamRegistration
+        fields = ['url', 'teamId']
